1ca1d09936e1d52b8ffad1fe643ba3cecedbe9d4
pedro <pedrohperoni@gmail.com>
2022-04-23 17:29:38 -0300

    create: basic file structure

---
+++ src/app.ts
@@ -0,0 +1,21 @@
+import cors from "cors";
+import dotenv from "dotenv";
+import express, { json } from "express";
+import "express-async-errors";
+import router from "../src/routers/index.js";
+import { errorHandlerMiddleware } from "./middlewares/errorHandlerMiddleware.js";
+
+
+
+const app = express();
+dotenv.config();
+
+app.use(json());
+app.use(cors());
+app.use(router);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Running on ${port}`);
+});
---
+++ src/controllers/userController.ts
@@ -0,0 +1,5 @@
+import { Request, Response } from "express";
+
+export async function userRegister(req: Request, res:Response){
+   console.log("controller")
+}
