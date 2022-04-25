4cedac8847a796d9f4fdcd6d0c8ef33fa1c3f40d
pedro <pedrohperoni@gmail.com>
2022-04-25 03:01:23 -0300

    feat: uppercase names

--- src/services/testsService.ts
+++ src/services/testsService.ts
@@ -13,12 +13,24 @@ export async function getByTeachers(){
 
    for(let i=0;i<teachers.length;i++){
       const categories = await testsRepository.getCategoryByTeacherId(teachers[i].id)
+      const upperCaseName = UpperCaseFirstLetterAllWords(teachers[i].name)
       const updatedCategories = {
          id: teachers[i].id,
-         teacher: teachers[i].name,
+         teacher: upperCaseName,
          categories
       }
       categoriesArray = [...categoriesArray, updatedCategories]
    }
    return categoriesArray
+}
+
+function UpperCaseFirstLetterAllWords(str: string){
+   const array = str.split(" ");
+   for (var i = 0; i < array.length; i++) {
+      array[i] = array[i].charAt(0).toUpperCase() + array[i].slice(1);
+  
+  }
+  const name = array.join(" ");
+  return name
+
 }
