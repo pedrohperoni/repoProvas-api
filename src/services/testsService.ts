3c8effcc38546ba5135664e4cc9e8591ba5d9354
pedro <pedrohperoni@gmail.com>
2022-04-25 12:23:10 -0300

    sort teachersTestsArray

--- src/services/testsService.ts
+++ src/services/testsService.ts
@@ -1,36 +1,66 @@
-import * as testsRepository from "../repositories/testsRepository.js"
+import * as testsRepository from "../repositories/testsRepository.js";
 
+type teachers = Array<{
+   id: number,
+   name: string
+}>
 
-export async function getByDisciplines(){
-   const tests = await testsRepository.getAllByDisciplines()
-   return tests
+export async function getByDisciplines() {
+  const tests = await testsRepository.getAllByDisciplines();
+  return tests;
 }
 
-export async function getByTeachers(){
-   const teachers = await testsRepository.getAllTeachers()
-   
-   let categoriesArray = []
+export async function getByTeachers() {
+   const teachers = await testsRepository.getAllTeachers();
+   const testsArray = sortTeachersArray(teachers);
+   return testsArray;
+}
 
-   for(let i=0;i<teachers.length;i++){
-      const categories = await testsRepository.getCategoryByTeacherId(teachers[i].id)
-      const upperCaseName = UpperCaseFirstLetterAllWords(teachers[i].name)
+async function sortTeachersArray(teachers: teachers) {
+   let teachersArray = [];
+   for (let i = 0; i < teachers.length; i++) {
+      const categories = await testsRepository.getCategoryByTeacherId(
+        teachers[i].id
+      );
+      const upperCaseName = UpperCaseFirstLetterAllWords(teachers[i].name);
       const updatedCategories = {
-         id: teachers[i].id,
-         teacher: upperCaseName,
-         categories
+        id: teachers[i].id,
+        teacher: upperCaseName,
+        categories,
+      };
+      teachersArray = [...teachersArray, updatedCategories];
+    }
+
+    let sortedTeachersArray = []
+    let test = {}
+    let teacher = {}
+
+    for(let i=0;i<teachersArray.length;i++){
+       let teacherTests = []
+      for(let j=0;j<teachersArray[i].categories.length;j++){
+         for(let k=0;k<teachersArray[i].categories[j].tests.length;k++){
+           test = {
+               id: teachersArray[i].categories[j].tests[k].id,
+               teacher: teachersArray[i].teacher,
+               category: teachersArray[i].categories[j].name,
+               test: teachersArray[i].categories[j].tests[k].name,
+               discipline: teachersArray[i].categories[j].tests[k].teachersDisciplines.disciplines.name
+            }
+            teacherTests.push(test)
+         }
       }
-      categoriesArray = [...categoriesArray, updatedCategories]
-   }
-   return categoriesArray
+      teacher = {teacher: teachersArray[i].teacher, teacherTests}
+      sortedTeachersArray.push(teacher)
+    }
+
+    return sortedTeachersArray
 }
 
-function UpperCaseFirstLetterAllWords(str: string){
-   const array = str.split(" ");
-   for (var i = 0; i < array.length; i++) {
-      array[i] = array[i].charAt(0).toUpperCase() + array[i].slice(1);
-  
+function UpperCaseFirstLetterAllWords(str: string) {
+  const array = str.split(" ");
+  for (var i = 0; i < array.length; i++) {
+    array[i] = array[i].charAt(0).toUpperCase() + array[i].slice(1);
   }
   const name = array.join(" ");
-  return name
-
+  return name;
 }
